ce8d680207b166b08322d6671e7b20ac590c26e6
Sinclebear <sanghyukj@gmail.com>
2022-05-02 18:08:24 +0900

    feat: mysql db 사용 준비

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,20 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOptions = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+
+  transform(value: any) {
+    value = value.toUpperCase();
+
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException(`${value} 값이 status 옵션에 없습니다.`);
+    }
+    return value;
+  }
+  private isStatusValid(status: any) {
+    // js의 indexOf는 일치하는 값이 없는 경우 -1 출력
+    const index = this.StatusOptions.indexOf(status);
+    return index !== -1;
+  }
+}
